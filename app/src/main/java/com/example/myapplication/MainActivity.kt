57ab7852a93c3a49e9c7b5a63537229eeb69080b
YurkinaSonya <yurkina.sonya@ya.ru>
2020-03-24 09:26:43 +0700

    ksldjhtfgsjhfgsdjhgfsljfgdsfgjh

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_black_login.*
+import kotlinx.android.synthetic.main.activity_light_login.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_black_login)
+        setContentView(R.layout.activity_light_login)
     }
     fun insert(v : View){
         if (Profile.login == log.text.toString() && Profile.password == pas.text.toString()){
@@ -19,11 +19,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(next)
             }
     }
-    fun regist(v : View){
-        var next:Intent = Intent(this@MainActivity, MainActivityRegistr::class.java)
+    fun regist(v : View) {
+        var next: Intent = Intent(this@MainActivity, MainActivityRegistr::class.java)
         next.putExtra("Registr", 1)
         startActivity(next)
     }
-    //d
-
 }
