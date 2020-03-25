654fdfd31dc7b3f0ef0d6d91c75b9d81495b6963
YurkinaSonya <yurkina.sonya@ya.ru>
2020-03-25 12:05:18 +0700

    ksldjhtfgsjhfgsdjhgfsljfgdsfgjha

--- app/src/main/java/com/example/myapplication/Desc.kt
+++ app/src/main/java/com/example/myapplication/Desc.kt
@@ -1,14 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import kotlin.properties.Delegates
 
 class Desc : AppCompatActivity() {
-
+    var n: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desc)
+        if (intent.extras?.get("a") != null)
+            n = intent.extras?.get("a").toString().toInt()
 
+        n = intent.extras?.get("a").toString().toInt()
+        //var s:Int = intent.getIntExtra("a", 0)
     }
 
+
 }
+
+
+
+
--- app/src/main/java/com/example/myapplication/Exercise.kt
+++ app/src/main/java/com/example/myapplication/Exercise.kt
@@ -2,13 +2,26 @@ package com.example.myapplication
 
 import android.content.Context
 import android.content.Intent
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class Excercise(val name: String?, var muscleGroup: Array<Muscle>,var imbDownLimit: Float? = null , var imbLimit: Float? = null) {
+class Excercise(
+    val name: String?,
+    var muscleGroup: Array<Muscle>,
+    var imbDownLimit: Float? = null ,
+    var imbLimit: Float? = null
+): Parcelable {
+    constructor(source: Parcel) : this(
+        source.readString(),
+        source.readArray(ClassLoader.getSystemClassLoader()) as Array<Muscle>,
+        source.readFloat(),
+        source.readFloat()
+    )
     //var defaultRest: Double = 30.0
     //var podxod: Int? = null
     //var povtor: Int? = null
@@ -30,11 +43,37 @@ class Excercise(val name: String?, var muscleGroup: Array<Muscle>,var imbDownLim
 
     fun checkDiseases(disease: Disease) = diseases.find { it !=  disease } != null
 
+    fun checkChoose(muscle: Muscle) = muscleGroup.find { it != muscle } != null
+
+    override fun writeToParcel(dest: Parcel?, flags: Int): Unit = with(dest) {
+        this?.writeString(name)
+        this?.writeArray(muscleGroup)
+        imbDownLimit?.let { this?.writeFloat(it) }
+        this?.writeArray(arrayOf(imbLimit))
+    }
+
     override fun toString(): String {
         return "$name"
     }
 
+    override fun describeContents(): Int {
+        return 0
+    }
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Excercise> = object : Parcelable.Creator<Excercise> {
+            override fun createFromParcel(parcel: Parcel): Excercise {
+                return Excercise(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Excercise?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
 }
+
+
 class ExerciseAdapter(): RecyclerView.Adapter<ExerciseAdapter.Companion.ExerciseHolder>(){
     var excercises : MutableList<Excercise> = mutableListOf()
     lateinit var c : Context
@@ -49,6 +88,7 @@ class ExerciseAdapter(): RecyclerView.Adapter<ExerciseAdapter.Companion.Exercise
     override fun onBindViewHolder(holder: ExerciseHolder, position: Int) {
         holder.n.setOnClickListener { v ->
             var next: Intent = Intent(c, Desc::class.java)
+            next.putExtra("a", excercises[position])
             c.startActivity(next)
         }
         holder.bind(excercises[position])
