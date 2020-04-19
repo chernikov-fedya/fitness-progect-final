1aa67c356cafc26f14001cd2ee380a190ae20801
chernikov-fedya <chernikov-fedya@mail.ru>
2020-04-19 17:34:52 +0700

    Train

--- app/src/main/java/com/example/myapplication/MainActivityMenu.kt
+++ app/src/main/java/com/example/myapplication/MainActivityMenu.kt
@@ -20,9 +20,32 @@ class MainActivityMenu : AppCompatActivity() {
         setContentView(R.layout.activity_main_menu)
 
         val fragmentAdapter = PagerAdapter(supportFragmentManager)
-        fragmentAdapter.data = mutableListOf(mutableListOf(TrainProgramm.listExcercise[4],TrainProgramm.listExcercise[4]),
-            mutableListOf(TrainProgramm.listExcercise[4],TrainProgramm.listExcercise[4]),
-            mutableListOf(TrainProgramm.listExcercise[4],TrainProgramm.listExcercise[4]))
+        // fragmentAdapter.data =
+        fun <T> createTrain(arr :Array<T>, count : Int, recommend : Int): MutableList<MutableList<T>> {
+            //Подгоняем количество рекомендуемых упражнений в день под все количество упражнений
+            var recommendEx = recommend
+            while (arr.size < recommendEx)
+                recommendEx--
+
+            // считаем количесто упражнений в день
+            var temp = arr.clone().toMutableList()
+            var quantity = temp.size / count
+
+            // если их меньше рекомендуемого, удваиваем
+            while (quantity < recommendEx) {
+                temp.addAll(arr.clone())
+                quantity = temp.size / count
+            }
+            // возвращаем массив массивов упражнений, собирая из из массива temp
+            return MutableList(count) {it ->
+                MutableList(recommendEx) {iti ->
+                    temp[it * recommendEx + iti]
+                }
+            }
+        }
+//        (mutableListOf(TrainProgramm.listExcercise[4],TrainProgramm.listExcercise[4]),
+//            mutableListOf(TrainProgramm.listExcercise[4],TrainProgramm.listExcercise[4]),
+//            mutableListOf(TrainProgramm.listExcercise[4],TrainProgramm.listExcercise[4]))
         viewpager.adapter = fragmentAdapter
 
         //tabs.setupWithViewPager(viewpager)
